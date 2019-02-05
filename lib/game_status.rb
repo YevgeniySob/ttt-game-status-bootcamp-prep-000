0d7ddbdb5b63ab98dd6713c0894172f3b3878526
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-05 07:38:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
     if(win_combo.all? {|index| (position_taken?(board,index))})
-      win_combo.all? {|ele|}
+      win_combo.all? {|ele| board[ele]}
       board[win_combo[0]] == board[win_combo[1]] == board[win_combo[2]]
     
   end
